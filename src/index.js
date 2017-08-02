7bbc7e25978d822af4becfc27fb20a687efa0802
Adam <adamh93@verizon.net>
2017-08-02 02:33:14 -0400

    done: Stored YouTube API Key in constant variable, created SearchBar component that contains an input field, added export statement for SearchBar in search_bar.js file, added import statement of SearchBar component from search_bar.js, modified App component to display SearchBar component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+// SearchBar component
+import SearchBar from './components/search_bar'
+// YouTube API KEY
+const API_KEY = 'AIzaSyBbHpdUnI2DcvI90du7cibUNqw0Wex8myY';
 
 
 // Create a new component. This component should produce
 // some HTML
 const App = function() {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-//Can also be done as '<App></App>'
-// document.querySelector() used to find div with class 'container'
-// then render the App component within the div with class 'container'
